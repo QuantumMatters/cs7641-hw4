839ca82699144f57bee0b87c56b64b7cbdcd5e97
QuantumMatters <kaseyevans42@gmail.com>
2021-11-20 07:09:54 -0500

    add code for defining the small maze problem

---
+++ src/main/java/edu/gatech/cs/mdp/experiments/SmallMaze.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs.mdp.experiments;
+
+import java.io.IOException;
+
+import edu.gatech.cs.mdp.problems.GridWorldProblem;
+
+public class SmallMaze {
+    GridWorldProblem gwp;
+
+    public SmallMaze(){
+        this.gwp = new GridWorldProblem("maze6.csv", 0.8, 50.0, -3.0, -1.0);
+    }
+    
+    public void runGammaExperiment() {
+
+        try {
+            gwp.valueIterationExperimenter("output_maze6_2/", new double[] {0.98, 0.99, 0.999, 0.9999}, 150, 1, 10000, 10, Math.pow(10,-5));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SmallMaze sm = new SmallMaze();
+        sm.runGammaExperiment();
+    }
+}
